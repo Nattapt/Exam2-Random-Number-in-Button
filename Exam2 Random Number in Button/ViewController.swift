946892edc33b38080ed1ecb45b45a240f02be9d6
Nattapat <san.1234@hotmail.com>
2018-10-11 22:47:09 +0700

    Initial Commit

---
+++ Exam2 Random Number in Button/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Exam2 Random Number in Button
+//
+//  Created by Nattapat on 11/10/2561 BE.
+//  Copyright © 2561 Nattapat. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
